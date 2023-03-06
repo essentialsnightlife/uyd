b2a49e9a988661bd4ef9127cba7dfcd5d217a04c
Edeman George <71378676+eds-101@users.noreply.github.com>
2023-03-06 16:08:21 +0000

    create function for bot responses

--- handler.ts
+++ handler.ts
@@ -1,6 +1,7 @@
 import { promptGenerator } from "./src/domains/ai/functions";
 import { sendAIQuestion } from "./src/domains/ai/functions";
 import { Constants } from "./constants";
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 
 const modelConfig = Constants.OPENAI_MODEL_CONFIG;
 
@@ -26,6 +27,35 @@ export async function analyser(event) {
       body: { result: completionText },
       input: event,
     };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      body: { error: error.message },
+      input: event,
+    };
+  }
+}
+
+const dbClient = new DynamoDBClient({ region: "eu-west-2" });
+const TableName = process.env.TABLE_NAME;
+export async function createBotResponse(event) {
+  const newBotResponse = {
+    id: { S: event.body.id },
+    userId: { S: event.body.userId },
+    query: { N: event.body.query },
+    response: { S: event.body.response },
+  };
+
+  try {
+    await dbClient.send(
+      new PutItemCommand({
+        TableName: TableName,
+        Item: newBotResponse,
+      })
+    );
+
+    return { statusCode: 200, body: JSON.stringify(newBotResponse) };
   } catch (error) {
     console.log(error);
 
