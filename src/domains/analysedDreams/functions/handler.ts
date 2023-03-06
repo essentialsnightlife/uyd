c50f03b7e7f7b9a3425a838584f7dfcd18d44beb
Edeman George <71378676+eds-101@users.noreply.github.com>
2023-03-06 20:53:45 +0000

    refactors

--- src/domains/analysedDreams/functions/handler.ts
+++ src/domains/analysedDreams/functions/handler.ts
@@ -1,54 +1,66 @@
-import { DynamoDBClient, PutItemCommand, GetItemCommand, GetItemCommandInput } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBClient,
+  PutItemCommand,
+    PutItemCommandInput,
+  GetItemCommand,
+  GetItemCommandInput,
+} from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { Constants } from "../../../../constants";
 
-const dbClient = new DynamoDBClient({ region: "eu-west-2" });
+import AWS_REGION = Constants.AWS_REGION;
+const dbClient = new DynamoDBClient({ region: AWS_REGION });
 const TableName = process.env.TABLE_NAME;
 export async function create(event) {
+
     const newAnalysedDream = {
-        id: { S: event.body.id },
-        userId: { S: event.body.userId },
-        query: { S: event.body.query },
-        response: { S: event.body.response },
-    };
+    id: { S: event.body.id },
+    userId: { S: event.body.userId },
+    query: { S: event.body.query },
+    response: { S: event.body.response },
+  };
 
-    try {
-        await dbClient.send(
-            new PutItemCommand({
-                TableName: TableName,
-                Item: newAnalysedDream,
-            })
-        );
-
-        return { statusCode: 200, body: JSON.stringify(newAnalysedDream) };
-    } catch (error) {
-        console.log(error);
-
-        return {
-            body: { error: error.message },
-            input: event,
-        };
+    const params: PutItemCommandInput = {
+        TableName: TableName,
+        Item: newAnalysedDream,
     }
+
+  try {
+    await dbClient.send(
+      new PutItemCommand(
+        params
+      )
+    );
+
+    return { statusCode: 200, body: JSON.stringify(newAnalysedDream) };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      body: { error: error.message },
+      input: event,
+    };
+  }
 }
 export async function get(event) {
+  const params: GetItemCommandInput = {
+    TableName: TableName,
+    Key: marshall({
+      id: event.queryStringParameters.id,
+    }),
+  };
 
-    const params: GetItemCommandInput = {
-        TableName: TableName,
-        Key: marshall({
-            id: event.queryStringParameters.id,
-        }),
+  try {
+    const results = await dbClient.send(new GetItemCommand(params));
+    console.log(results);
+
+    return { statusCode: 200, body: JSON.stringify(unmarshall(results.Item)) };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      body: { error: error.message },
+      input: event,
     };
-
-    try {
-        const results = await dbClient.send(new GetItemCommand(params));
-        console.log(results)
-
-        return { statusCode: 200, body: JSON.stringify(unmarshall(results.Item)) };
-    } catch (error) {
-        console.log(error);
-
-        return {
-            body: { error: error.message },
-            input: event,
-        };
-    }
+  }
 }
