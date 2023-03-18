d791fdb59c13a91583d4baabe0d9e1aeb0541f66
Edeman George <71378676+eds-101@users.noreply.github.com>
2023-03-18 12:03:29 +0000

    react 18

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,13 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+
+// @ts-ignore
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>,
 );
