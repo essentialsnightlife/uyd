8cfee1ac8e25ec16419712d0fe605832dd7e79d9
Edeman George <71378676+eds-101@users.noreply.github.com>
2023-03-18 13:54:59 +0000

    wip analyser page

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,42 +1,40 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [question, setQuestion] = useState('');
+  const [response, setResponse] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
         <p className="header">Dream Analyser</p>
       </header>
       <div className="body">
-
         <div className="question-box">
-          <textarea placeholder="Ask a question" />
+          <textarea
+            placeholder="Ask a question"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+
           <button>Ask</button>
         </div>
 
-          <div className="response-box">
-              <p>Response 🧠</p>
-          </div>
+        <div className="response-box">
+            {response ? (<p>Response 🧠: {response}</p>) : <p>Waiting 🧠</p>}
+        </div>
 
-          <p> Placeholder: used your daily quota of queries</p>
+        <p> Placeholder: used your daily quota of queries</p>
       </div>
 
       <div className="footer">
         <p>
-          <a
-            className="App-link"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="#" target="_blank" rel="noopener noreferrer">
             Leave Feedback
           </a>
           {' | '}
-          <a
-            className="App-link"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="#" target="_blank" rel="noopener noreferrer">
             Peng Devs
           </a>
         </p>
