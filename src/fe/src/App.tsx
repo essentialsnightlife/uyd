59046466c968e332c426d6cc543adef1f3bc0998
Edeman George <71378676+eds-101@users.noreply.github.com>
2023-03-26 22:40:55 +0100

    page refactor

--- src/fe/src/App.tsx
+++ src/fe/src/App.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import { FormEvent, useState } from 'react';
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
 import './App.css';
-import Footer from './Footer';
+import Layout from './Layout';
 
 function App() {
   const [question, setQuestion] = useState('');
@@ -30,40 +29,41 @@ function App() {
   };
 
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Dream Analyser
-        </Typography>
-        <Typography sx={{ mt: 1, mb: 3 }} color="text.secondary">
-          ☁️ Start here | 🧠 Use your dreams
-        </Typography>
-      </Box>
-      <Box sx={{ my: 4 }}>
-        <form className="question-box" onSubmit={(e) => handleSubmit(e, question)}>
-          <TextareaAutosize
+    <Layout>
+      <>
+        <Box sx={{ my: 4 }}>
+          <form className="question-box" onSubmit={(e) => handleSubmit(e, question)}>
+            <TextareaAutosize
               aria-label="minimum height"
               minRows={3}
               placeholder="Describe your dream here..."
               value={question}
-              onChange={(e) => setQuestion(e.target.value)} nonce={undefined} onResize={undefined}
-              onResizeCapture={undefined}          />
-        </form>
-        <Button variant="text" size="large" style={{ color: 'black' }} sx={{
-            ':hover': {
+              onChange={(e) => setQuestion(e.target.value)}
+              nonce={undefined}
+              onResize={undefined}
+              onResizeCapture={undefined}
+            />
+          </form>
+          <Button
+            variant="text"
+            size="large"
+            style={{ color: 'black' }}
+            sx={{
+              ':hover': {
                 bgcolor: 'grey',
-            },
-        }}>
-          Go
-        </Button>
-      </Box>
-      <Box>
-        <Typography variant="h6">
-          {response ? `Response 🧠: ${response}` : null}
-        </Typography>
-      </Box>
-      <Footer />
-    </Container>
+              },
+            }}
+          >
+            Go
+          </Button>
+        </Box>
+        <Box>
+          <Typography variant="h6">
+            {response ? `Response 🧠: ${response}` : null}
+          </Typography>
+        </Box>
+      </>
+    </Layout>
   );
 }
 
