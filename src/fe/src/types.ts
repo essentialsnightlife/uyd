9df6a1faf874902f4f3a0324049c274f1f05af00
Edeman George <71378676+eds-101@users.noreply.github.com>
2023-04-20 21:05:37 +0100

    type update

--- src/fe/src/types.ts
+++ src/fe/src/types.ts
@@ -1,7 +1,7 @@
 export type AnsweredQuery = {
   id: string;
-  userId?: string | undefined;
-  date?: string;
+  userId: string | undefined;
+  date: string;
   query: string;
   response: string;
 };
