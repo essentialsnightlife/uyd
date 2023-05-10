e38e8507377cb7279230594f8bc71baedc717e89
Edeman George <71378676+eds-101@users.noreply.github.com>
2023-05-10 22:38:47 +0100

    api refactors

--- src/fe/src/apis.ts
+++ src/fe/src/apis.ts
@@ -1,44 +1,8 @@
-import { Session } from '@supabase/supabase-js';
-
-import { AnsweredQuery } from './types';
-
-function removeNonLetters(str: string) {
-  return str.replace(/^[^a-zA-Z]*/g, '');
-}
-
-export async function saveAnsweredQuery({
-  session,
-  query,
-  analysedDream,
-}: {
-  session: Session | null;
-  query: string;
-  analysedDream: string;
-}) {
-  const formattedAnsweredQuery: AnsweredQuery = {
-    id: 'UYD' + Date.now(),
-    userId: session?.user.id,
-    query,
-    response: analysedDream,
-    date: new Date().toISOString(),
-  };
-
-  const response = await fetch(
-    'https://d3xxs9kqk8.execute-api.eu-west-2.amazonaws.com/dreams',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formattedAnsweredQuery),
-    },
-  );
-  const data = await response.json();
-  console.log(data);
-  if (data.body.error) {
-    throw new Error(data);
-  }
-  return { data, answeredQuery: formattedAnsweredQuery };
-}
 export async function analyseDream(query: string) {
+  function removeNonLetters(str: string) {
+    return str.replace(/^[^a-zA-Z]*/g, '');
+  }
+
   let response;
   let data;
   try {
@@ -52,7 +16,7 @@ export async function analyseDream(query: string) {
     );
     data = await response.json();
     console.log('query: ', query);
-    console.log('data gott: ', data);
+    console.log('data: ', data);
   } catch (err: unknown) {
     console.log('api error');
     console.log(err);
@@ -65,4 +29,43 @@ export async function analyseDream(query: string) {
     throw new Error(data.body.error);
   }
   return removeNonLetters(data.body.result);
+}
+
+export const getUsersDreams = async (id: string) => {
+  try {
+    const response = await fetch(
+      'https://d3xxs9kqk8.execute-api.eu-west-2.amazonaws.com/dreams/' + id || '',
+      // 'user123',
+    );
+    const result = await response.json();
+    console.log('result', result);
+    return result;
+  } catch (err) {
+    console.log(err);
+    throw new Error(
+      'Sorry, something went wrong. Please refresh the page and try again. 🙏',
+    );
+  }
+};
+
+export async function deleteDream(dreamId: string) {
+  try {
+    const response = await fetch(
+      'https://d3xxs9kqk8.execute-api.eu-west-2.amazonaws.com/dreams/' + dreamId,
+      {
+        method: 'DELETE',
+      },
+    );
+    const result = await response.json();
+    if (result.error) {
+      console.log(result.error);
+    }
+    console.log('result', result);
+    alert('Dream deleted! 👋');
+    return result;
+  } catch (err) {
+    console.log('Error deleting dream 🚨');
+    console.log(err);
+    throw new Error('Sorry, we could not delete your dream. Please try again. 🙏');
+  }
 }
